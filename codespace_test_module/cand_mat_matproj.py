51072eee208396d351d8ef09d12901e9d75dc509
056wagomu <72100310+056wagomu@users.noreply.github.com>
2024-06-25 15:29:54 +0900

    new file:   candidate_material_list/Al_candidate_material_list.csv 	new file:   candidate_material_list/Al_is_stable_candidate_material_list.csv 	modified:   candidate_material_list/B_candidate_material_list.csv 	new file:   candidate_material_list/B_is_stable_candidate_material_list.csv 	deleted:    candidate_material_list/Ba_candidate_material_list.csv 	new file:   candidate_material_list/C_candidate_material_list.csv 	new file:   candidate_material_list/C_is_stable_candidate_material_list.csv 	new file:   candidate_material_list/Li_candidate_material_list.csv 	new file:   candidate_material_list/Li_is_stable_candidate_material_list.csv 	modified:   candidate_material_list/Ni_candidate_material_list.csv 	new file:   candidate_material_list/Ni_is_stable_candidate_material_list.csv 	new file:   candidate_material_list/Si_candidate_material_list.csv 	new file:   candidate_material_list/Si_is_stable_candidate_material_list.csv 	deleted:    candidate_material_list/Sr_candidate_material_list.csv 	modified:   codespace_test_module/cand_mat_matproj.py

--- codespace_test_module/cand_mat_matproj.py
+++ codespace_test_module/cand_mat_matproj.py
@@ -1,20 +1,19 @@
-#materials projectからNNに突っ込んで予測するための
-#情報を取得するためのソースコード
-#ひとまずホウ化物？とかになるのかなという感じ
-#例えばホウ化物で構成元素が3つ以上とか
+# materials projectからNNに突っ込んで予測するための
+# 情報を取得するためのソースコード
+# ひとまずホウ化物？とかになるのかなという感じ
+# 例えばホウ化物で構成元素が3つ以上とか
 
 # %%
 import os
-import pickle
+
 import pandas as pd
-from chempy import Substance
 import periodictable
-import ast
+from chempy import Substance
+from mp_api.client import MPRester
+# import ast
 
 pd.options.display.max_columns = 100
 pd.options.display.max_rows = 100
-
-from mp_api.client import MPRester
 
 MY_API_KEY = "09vnTU6R8jWfPhuZ6bFs3niTwe1F03x4"
 
@@ -25,45 +24,102 @@ if not os.path.exists(path_output_dir):
     os.makedirs(path_output_dir)
 
 
-
 # %%
 # define physical properties/infos you want to obtain
 properties = ["material_id", "formula_pretty", "band_gap", "efermi", "energy_above_hull"]
-#nameは*の数で構成する元素数を指定できる？
+# nameは*の数で構成する元素数を指定できる？
 name = "**"
+_is_stable = True
 
-#他にはBa, Sr, Niとか?
+# 他にはBa, Sr, Niとか?
+# 上原先生の助言の元,C,Al,Si,Li,B,Niで半導体周りでやってみる
+# 酸素を除外したデータでもやってみる
+# 酸素除外について、あまり意味がないというのと
+# 窒化物を研究室で作ることが出来ないのでこれを除外する
 
-element_filter = "Ni"
+element_filter = ["C", "Al", "Si", "Li", "B", "Ni"]
+
 
 with MPRester(MY_API_KEY) as mpr:
-    results = mpr.materials.summary.search(fields=properties, elements = [element_filter])
+    for atm in element_filter:
+        results = mpr.materials.summary.search(fields=properties, elements=[atm], exclude_elements=["Ba", "Cu", "Y", "Fe", "Hg", "N"], is_stable=_is_stable)
+
+        cand_df = pd.DataFrame(columns=["formula", "material_id", "band_gap", "efermi", "energy_above_hull"])
+
+        for mat in results:
+            subdata = {"formula": mat.formula_pretty, "material_id": mat.material_id, "band_gap": mat.band_gap, "efermi": mat.efermi, "energy_above_hull": mat.energy_above_hull}
+
+            subdf = pd.DataFrame(subdata, index=[0])
+
+            cand_df = pd.concat([cand_df, subdf], axis=0)
+
+        cand_df = cand_df.reset_index(drop=True)
+        cand_df["comp_atoms"] = cand_df["formula"].apply(lambda x: Substance.from_formula(x).composition)
+        cand_df["sorted_keys_comp_atoms"] = cand_df["formula"].apply(lambda x: sorted(Substance.from_formula(x).composition.keys()))
+        cand_df["comp_number"] = cand_df.apply(lambda row: "".join(f'{k}{row["comp_atoms"][k]}' for k in row["sorted_keys_comp_atoms"]), axis=1)
+
+        chosen_material_list = []
+
+        for i, subdf in cand_df.groupby("comp_number"):
+            if len(subdf) > 1:
+                subdf = subdf.sort_values("energy_above_hull")
+                chosen_material_list.append(subdf.index[0])
+
+            else:
+                chosen_material_list.append(subdf.index[0])
+
+        chosen_cand_df = cand_df.loc[chosen_material_list]
+
+        chosen_cand_df = chosen_cand_df.dropna(subset=["efermi"])
+
+        chosen_cand_df = chosen_cand_df.reset_index(drop=True)
+
+        chosen_cand_df["new_formula"] = None
+
+        for i in range(len(chosen_cand_df)):
+            dict_keys = list(chosen_cand_df.loc[i, "comp_atoms"].keys())
+
+            comp = ""
+
+            for j in range(len(dict_keys)):
+                element = str(periodictable.elements[dict_keys[j]])
+                comp += element + str(chosen_cand_df.loc[i, "comp_atoms"][dict_keys[j]])
+
+            chosen_cand_df.loc[i, "new_formula"] = comp
+
+        # except Exception as e:
+        # print(e)
+        # print("Error: ", i, j)
+
+        display(chosen_cand_df)
+
+        # できたもの二つ目も保存しましょうね
+        chosen_cand_df.to_csv("/workspaces/codespace_practice/candidate_material_list/" + atm + "_is_stable" + "_candidate_material_list.csv", index=False)
 
 
 # %%
-#この得たデータをpickleで保存する理由が!ありません!
-#地産地消でcsvにしよう!
-cand_df = pd.DataFrame(columns = ["formula", "material_id", "band_gap", "efermi", "energy_above_hull"])
+dict = {5: 10, 1: 11, 6: 2}
+dict_key = dict.keys()
+keys_list = list(dict_key)
+print(keys_list)
+
+
+# %%
+# この得たデータをpickleで保存する理由が!ありません!
+# 地産地消でcsvにしよう!
+cand_df = pd.DataFrame(columns=["formula", "material_id", "band_gap", "efermi", "energy_above_hull"])
 for mat in results:
-    subdata = {
-        "formula": mat.formula_pretty, 
-        "material_id": mat.material_id, 
-        "band_gap": mat.band_gap, 
-        "efermi": mat.efermi, 
-        "energy_above_hull": mat.energy_above_hull
-    }
-    
+    subdata = {"formula": mat.formula_pretty, "material_id": mat.material_id, "band_gap": mat.band_gap, "efermi": mat.efermi, "energy_above_hull": mat.energy_above_hull}
+
     subdf = pd.DataFrame(subdata, index=[0])
-    
+
     cand_df = pd.concat([cand_df, subdf], axis=0)
 
 
-
-# %%
-cand_df = cand_df.reset_index(drop = True)
+cand_df = cand_df.reset_index(drop=True)
 cand_df["comp_atoms"] = cand_df["formula"].apply(lambda x: Substance.from_formula(x).composition)
 cand_df["sorted_keys_comp_atoms"] = cand_df["formula"].apply(lambda x: sorted(Substance.from_formula(x).composition.keys()))
-cand_df["comp_number"] = cand_df.apply(lambda row: ''.join(f'{k}{row["comp_atoms"][k]}' for k in row["sorted_keys_comp_atoms"]), axis=1)
+cand_df["comp_number"] = cand_df.apply(lambda row: "".join(f'{k}{row["comp_atoms"][k]}' for k in row["sorted_keys_comp_atoms"]), axis=1)
 
 chosen_material_list = []
 
@@ -71,7 +127,7 @@ for i, subdf in cand_df.groupby("comp_number"):
     if len(subdf) > 1:
         subdf = subdf.sort_values("energy_above_hull")
         chosen_material_list.append(subdf.index[0])
-        
+
     else:
         chosen_material_list.append(subdf.index[0])
 
@@ -79,19 +135,16 @@ chosen_cand_df = cand_df.loc[chosen_material_list]
 
 chosen_cand_df = chosen_cand_df.dropna(subset=["efermi"])
 
-chosen_cand_df = chosen_cand_df.reset_index(drop = True)
-
-
-
-# %%
-#できたものは保存しましょうね～
-#chosen_cand_df.to_csv("/workspaces/codespace_practice/candidate_material_list/candidate_material_list.csv", index=False)
-#chosen_cand_df = pd.read_csv("/workspaces/codespace_practice/candidate_material_list/candidate_material_list.csv")
+chosen_cand_df = chosen_cand_df.reset_index(drop=True)
 
 
 # %%
-#どうやら一度保存してから再読み込みの形を取ると辞書型が崩れて文字型になるので以下の処理が必要
-chosen_cand_df["comp_atoms"] = chosen_cand_df["comp_atoms"].apply(ast.literal_eval)
+# できたものは保存しましょうね～
+# chosen_cand_df.to_csv("/workspaces/codespace_practice/candidate_material_list/candidate_material_list.csv", index=False)
+# chosen_cand_df = pd.read_csv("/workspaces/codespace_practice/candidate_material_list/candidate_material_list.csv")
+
+# どうやら一度保存してから再読み込みの形を取ると辞書型が崩れて文字型になるので以下の処理が必要
+# chosen_cand_df["comp_atoms"] = chosen_cand_df["comp_atoms"].apply(ast.literal_eval)
 
 
 # %%
@@ -102,22 +155,20 @@ try:
         dict_keys = list(chosen_cand_df.loc[i, "comp_atoms"].keys())
 
         comp = ""
-        
+
         for j in range(len(dict_keys)):
             element = str(periodictable.elements[dict_keys[j]])
             comp += element + str(chosen_cand_df.loc[i, "comp_atoms"][dict_keys[j]])
-            
+
         chosen_cand_df.loc[i, "new_formula"] = comp
-        
+
 except Exception as e:
     print(e)
     print("Error: ", i, j)
 
 
-
-# %%
 display(chosen_cand_df)
-# %%
-#できたもの二つ目も保存しましょうね
+
+# できたもの二つ目も保存しましょうね
 chosen_cand_df.to_csv("/workspaces/codespace_practice/candidate_material_list/" + element_filter + "_candidate_material_list.csv", index=False)
 # %%
