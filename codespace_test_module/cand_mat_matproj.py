2c2364182f6f62c66a97ecddadf251afad0ba68a
056wagomu <72100310+056wagomu@users.noreply.github.com>
2024-07-02 16:00:51 +0900

    deleted:    candidate_material_list/Al_candidate_material_list.csv 	modified:   candidate_material_list/Al_is_stable_candidate_material_list.csv 	deleted:    candidate_material_list/B_candidate_material_list.csv 	modified:   candidate_material_list/B_is_stable_candidate_material_list.csv 	deleted:    candidate_material_list/C_candidate_material_list.csv 	modified:   candidate_material_list/C_is_stable_candidate_material_list.csv 	new file:   candidate_material_list/Ca_is_stable_candidate_material_list.csv 	new file:   candidate_material_list/K_is_stable_candidate_material_list.csv 	deleted:    candidate_material_list/Li_candidate_material_list.csv 	modified:   candidate_material_list/Li_is_stable_candidate_material_list.csv 	new file:   candidate_material_list/Mg_is_stable_candidate_material_list.csv 	deleted:    candidate_material_list/Ni_candidate_material_list.csv 	modified:   candidate_material_list/Ni_is_stable_candidate_material_list.csv 	deleted:    candidate_material_list/Si_candidate_material_list.csv 	modified:   candidate_material_list/Si_is_stable_candidate_material_list.csv 	new file:   candidate_material_list_final/Al_is_stable_candidate_material_list.csv 	new file:   candidate_material_list_final/B_is_stable_candidate_material_list.csv 	new file:   candidate_material_list_final/C_is_stable_candidate_material_list.csv 	new file:   candidate_material_list_final/Ca_is_stable_candidate_material_list.csv 	new file:   candidate_material_list_final/K_is_stable_candidate_material_list.csv 	new file:   candidate_material_list_final/Li_is_stable_candidate_material_list.csv 	new file:   candidate_material_list_final/Mg_is_stable_candidate_material_list.csv 	new file:   candidate_material_list_final/Ni_is_stable_candidate_material_list.csv 	new file:   candidate_material_list_final/Si_is_stable_candidate_material_list.csv 	modified:   codespace_test_module/cand_mat_matproj.py 	modified:   codespace_test_module/main.py

--- codespace_test_module/cand_mat_matproj.py
+++ codespace_test_module/cand_mat_matproj.py
@@ -17,16 +17,34 @@ pd.options.display.max_rows = 100
 
 MY_API_KEY = "09vnTU6R8jWfPhuZ6bFs3niTwe1F03x4"
 
-# %%
 # Output directory
-path_output_dir = "/workspaces/codespace_practice/candidate_material_list/"
+path_output_dir = "/workspaces/codespace_practice/candidate_material_list_final/"
 if not os.path.exists(path_output_dir):
     os.makedirs(path_output_dir)
+    
+    
+    
+# %%
+with MPRester(MY_API_KEY) as mpr:
+    res = mpr.materials.summary.search(formula = "SrTiO3", is_stable=True)
+    #print(res)
+
+if len(res[0].database_IDs) != 0:
+    ID_bool = True
+else:
+    ID_bool = False
+print(ID_bool)
+print(res[0].database_IDs)
+keys = res[0].database_IDs.keys()
+for key in res[0].database_IDs.keys():
+    print(res[0].database_IDs[key])
+#print(res[0].database_IDs[keys][0])
+
 
 
 # %%
 # define physical properties/infos you want to obtain
-properties = ["material_id", "formula_pretty", "band_gap", "efermi", "energy_above_hull"]
+properties = ["material_id", "formula_pretty", "band_gap", "efermi", "energy_above_hull", "database_IDs"]
 # nameは*の数で構成する元素数を指定できる？
 name = "**"
 _is_stable = True
@@ -37,21 +55,26 @@ _is_stable = True
 # 酸素除外について、あまり意味がないというのと
 # 窒化物を研究室で作ることが出来ないのでこれを除外する
 
-element_filter = ["C", "Al", "Si", "Li", "B", "Ni"]
+element_filter = ["C", "Al", "Si", "Li", "B", "Ni", "Mg", "Ca", "K"]
 
 
 with MPRester(MY_API_KEY) as mpr:
     for atm in element_filter:
-        results = mpr.materials.summary.search(fields=properties, elements=[atm], exclude_elements=["Ba", "Cu", "Y", "Fe", "Hg", "N"], is_stable=_is_stable)
+        results = mpr.materials.summary.search(fields=properties, elements=[atm], is_stable=_is_stable)
 
-        cand_df = pd.DataFrame(columns=["formula", "material_id", "band_gap", "efermi", "energy_above_hull"])
+        cand_df = pd.DataFrame(columns=["formula", "material_id", "band_gap", "efermi", "energy_above_hull", "database_IDs"])
 
         for mat in results:
-            subdata = {"formula": mat.formula_pretty, "material_id": mat.material_id, "band_gap": mat.band_gap, "efermi": mat.efermi, "energy_above_hull": mat.energy_above_hull}
+            if len(mat.database_IDs) != 0:
+                database_keys = mat.database_IDs.keys()
+                for key in database_keys:
+                    database_ID = mat.database_IDs[key][0]
+                    break
+                
+                subdata = {"formula": mat.formula_pretty, "material_id": mat.material_id, "band_gap": mat.band_gap, "efermi": mat.efermi, "energy_above_hull": mat.energy_above_hull, "database_IDs": database_ID}
+                subdf = pd.DataFrame(subdata, index=[0])
 
-            subdf = pd.DataFrame(subdata, index=[0])
-
-            cand_df = pd.concat([cand_df, subdf], axis=0)
+                cand_df = pd.concat([cand_df, subdf], axis=0)
 
         cand_df = cand_df.reset_index(drop=True)
         cand_df["comp_atoms"] = cand_df["formula"].apply(lambda x: Substance.from_formula(x).composition)
@@ -70,7 +93,7 @@ with MPRester(MY_API_KEY) as mpr:
 
         chosen_cand_df = cand_df.loc[chosen_material_list]
 
-        chosen_cand_df = chosen_cand_df.dropna(subset=["efermi"])
+        #chosen_cand_df = chosen_cand_df.dropna(subset=["efermi"])
 
         chosen_cand_df = chosen_cand_df.reset_index(drop=True)
 
@@ -87,88 +110,12 @@ with MPRester(MY_API_KEY) as mpr:
 
             chosen_cand_df.loc[i, "new_formula"] = comp
 
-        # except Exception as e:
-        # print(e)
-        # print("Error: ", i, j)
+        #display(chosen_cand_df)
+        #print(chosen_cand_df.isnull().sum())
+        
+        #break
 
-        display(chosen_cand_df)
-
-        # できたもの二つ目も保存しましょうね
-        chosen_cand_df.to_csv("/workspaces/codespace_practice/candidate_material_list/" + atm + "_is_stable" + "_candidate_material_list.csv", index=False)
+        chosen_cand_df.to_csv(path_output_dir + atm + "_is_stable_candidate_material_list.csv", index=False)
 
 
-# %%
-dict = {5: 10, 1: 11, 6: 2}
-dict_key = dict.keys()
-keys_list = list(dict_key)
-print(keys_list)
-
-
-# %%
-# この得たデータをpickleで保存する理由が!ありません!
-# 地産地消でcsvにしよう!
-cand_df = pd.DataFrame(columns=["formula", "material_id", "band_gap", "efermi", "energy_above_hull"])
-for mat in results:
-    subdata = {"formula": mat.formula_pretty, "material_id": mat.material_id, "band_gap": mat.band_gap, "efermi": mat.efermi, "energy_above_hull": mat.energy_above_hull}
-
-    subdf = pd.DataFrame(subdata, index=[0])
-
-    cand_df = pd.concat([cand_df, subdf], axis=0)
-
-
-cand_df = cand_df.reset_index(drop=True)
-cand_df["comp_atoms"] = cand_df["formula"].apply(lambda x: Substance.from_formula(x).composition)
-cand_df["sorted_keys_comp_atoms"] = cand_df["formula"].apply(lambda x: sorted(Substance.from_formula(x).composition.keys()))
-cand_df["comp_number"] = cand_df.apply(lambda row: "".join(f'{k}{row["comp_atoms"][k]}' for k in row["sorted_keys_comp_atoms"]), axis=1)
-
-chosen_material_list = []
-
-for i, subdf in cand_df.groupby("comp_number"):
-    if len(subdf) > 1:
-        subdf = subdf.sort_values("energy_above_hull")
-        chosen_material_list.append(subdf.index[0])
-
-    else:
-        chosen_material_list.append(subdf.index[0])
-
-chosen_cand_df = cand_df.loc[chosen_material_list]
-
-chosen_cand_df = chosen_cand_df.dropna(subset=["efermi"])
-
-chosen_cand_df = chosen_cand_df.reset_index(drop=True)
-
-
-# %%
-# できたものは保存しましょうね～
-# chosen_cand_df.to_csv("/workspaces/codespace_practice/candidate_material_list/candidate_material_list.csv", index=False)
-# chosen_cand_df = pd.read_csv("/workspaces/codespace_practice/candidate_material_list/candidate_material_list.csv")
-
-# どうやら一度保存してから再読み込みの形を取ると辞書型が崩れて文字型になるので以下の処理が必要
-# chosen_cand_df["comp_atoms"] = chosen_cand_df["comp_atoms"].apply(ast.literal_eval)
-
-
-# %%
-chosen_cand_df["new_formula"] = None
-
-try:
-    for i in range(len(chosen_cand_df)):
-        dict_keys = list(chosen_cand_df.loc[i, "comp_atoms"].keys())
-
-        comp = ""
-
-        for j in range(len(dict_keys)):
-            element = str(periodictable.elements[dict_keys[j]])
-            comp += element + str(chosen_cand_df.loc[i, "comp_atoms"][dict_keys[j]])
-
-        chosen_cand_df.loc[i, "new_formula"] = comp
-
-except Exception as e:
-    print(e)
-    print("Error: ", i, j)
-
-
-display(chosen_cand_df)
-
-# できたもの二つ目も保存しましょうね
-chosen_cand_df.to_csv("/workspaces/codespace_practice/candidate_material_list/" + element_filter + "_candidate_material_list.csv", index=False)
 # %%
--- codespace_test_module/main.py
+++ codespace_test_module/main.py
@@ -4,17 +4,6 @@ import os
 
 MY_API_KEY="09vnTU6R8jWfPhuZ6bFs3niTwe1F03x4"
 
-#with MPRester( MY_API_KEY ) as mpr:
-#  list_of_available_fields = mpr.materials.summary.available_fields
-#print(list_of_available_fields)
-#
-#['builder_meta', 'nsites', 'elements', 'nelements', 'composition', 'composition_reduced', 'formula_pretty', 'formula_anonymous', 'chemsys', 'volume', 'density', 'density_atomic', 'symmetry', 'property_name', 'material_id', 'deprecated', 'deprecation_reasons', 'last_updated', 'origins', 'warnings', 'structure', 'task_ids', 'uncorrected_energy_per_atom', 'energy_per_atom', 'formation_energy_per_atom', 'energy_above_hull', 'is_stable', 'equilibrium_reaction_energy_per_atom', 'decomposes_to', 'xas', 'grain_boundaries', 'band_gap', 'cbm', 'vbm', 'efermi', 'is_gap_direct', 'is_metal', 'es_source_calc_id', 'bandstructure', 'dos', 'dos_energy_up', 'dos_energy_down', 'is_magnetic', 'ordering', 'total_magnetization', 'total_magnetization_normalized_vol', 'total_magnetization_normalized_formula_units', 'num_magnetic_sites', 'num_unique_magnetic_sites', 'types_of_magnetic_species', 'bulk_modulus', 'shear_modulus', 'universal_anisotropy', 'homogeneous_poisson', 'e_total', 'e_ionic', 'e_electronic', 'n', 'e_ij_max', 'weighted_surface_energy_EV_PER_ANG2', 'weighted_surface_energy', 'weighted_work_function', 'surface_anisotropy', 'shape_factor', 'has_reconstructed', 'possible_species', 'has_props', 'theoretical', 'database_IDs']
-
-# See https://api.materialsproject.org/redoc
-# or  https://api.materialsproject.org/docs#/Materials%20Summary/search_materials_summary__get
-# or  https://docs.materialsproject.org/downloading-data/using-the-api
-
-
 #Get cif files of ABO3 systems
 # Only for stable insulators with band_gap < 1 and spacegroup = Pm-3m
 band_gap_min = None
@@ -29,35 +18,16 @@ _is_metal=False
 #_is_stable=True
 #_is_metal=True
 
-# define physical properties/infos you want to obtain
-properties = ['formula_pretty','material_id','structure','symmetry','is_metal','band_gap']
-name = "**O3"
-with MPRester( MY_API_KEY ) as mpr:
-    results = mpr.materials.summary.search(formula=name, band_gap=(band_gap_min, band_gap_max), spacegroup_symbol = sg_symb, is_stable=_is_stable, is_metal=_is_metal, fields=properties)
-
-
-#Output
-path_output_dir = "./ABO3_cif/"
-if not os.path.exists(path_output_dir):
-    os.makedirs(path_output_dir)
-for mat in results:
-    #print(mat.material_id, mat.formula_pretty)
-    #print(mat.symmetry.symbol)
-    #print(mat.is_metal, mat.band_gap)
-    ofile = path_output_dir+mat.material_id+"_"+mat.formula_pretty+".cif"
-    mat.structure.to(filename = ofile)
-
 #Get physical properties of SrBO3 systems (B="V", "Ti", or "Co")
 # define physical properties/infos you want to obtain
-properties = ['formula_pretty', "band_gap", "formation_energy_per_atom", "symmetry"]
-name = "**O3"
-with MPRester( MY_API_KEY ) as m:
-    for atm in ["V","Ti","Co"]:
-        results = mpr.materials.summary.search(formula=name, elements=[atm, "Sr"], fields=properties)
+properties = ['formula_pretty', "band_gap", "formation_energy_per_atom", "symmetry", "efermi"]
+name = "NaAlH4"
+with MPRester( MY_API_KEY ) as mpr:
+    results = mpr.materials.summary.search(formula=name, fields=properties)
 
-        #Output
-        for mat in results:
-            print(mat.formula_pretty)
-            print("  band_gap:", mat.band_gap)
-            print("  formation_energy_per_atom:", mat.formation_energy_per_atom)
-            print("  symmetry:", mat.symmetry)
+    for mat in results:
+        print(mat.formula_pretty)
+        print("  band_gap:", mat.band_gap)
+        print("  formation_energy_per_atom:", mat.formation_energy_per_atom)
+        print("  symmetry:", mat.symmetry)
+        print("  efermi:", mat.efermi)
